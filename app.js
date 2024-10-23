d692dc351ba4aecf94dc89585ef0e8ae18a525f4
silviofbarbieri <silviofbarbieri@gmail.com>
2024-10-23 16:24:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,18 +9,19 @@ document.addEventListener('DOMContentLoaded', function () {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
-
-  // Create a marker with popup and add it to the map
-  var marker = L.marker([-23.516357107383552, -46.835143207955774]).addTo(map);
-  marker.bindPopup("Amada turma de DMD, você está aqui, olha que legal!").openPopup();
-
-var greenIcon = L.icon({
+  var greenIcon = L.icon({
     iconUrl: 'https://t3.ftcdn.net/jpg/02/73/16/24/240_F_273162497_ShAAB1TH0vhM4UUWbhBuao8jtGBDubwD.jpg',
 
     iconSize:     [80, 60], // size of the icon
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
+
+  // Create a marker with popup and add it to the map
+  var marker = L.marker([-23.516357107383552, -46.835143207955774]).addTo(map);
+  marker.bindPopup("Amada turma de DMD, você está aqui, olha que legal!").openPopup();
+
+
   
     // Create a marker with popup and add it to the map
   var marker2 = L.marker([-23.52026413037824, -46.83670586136634], {icon: greenIcon}).addTo(map);
