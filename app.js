c20724a03c70ebe71c987b0e128258444bb5cc0d
silviofbarbieri <silviofbarbieri@gmail.com>
2024-10-24 11:10:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -27,4 +27,17 @@ document.addEventListener('DOMContentLoaded', function () {
   var marker2 = L.marker([-23.52026413037824, -46.83670586136634], {icon: greenIcon}).addTo(map);
   marker2.bindPopup("Amada turma de DMD, eu me perdi aqui").openPopup();
 
+var markersLayer = new L.LayerGroup();
+map.addControl( new L.Control.ListMarkers({layer: markersLayer}) );
+  
+var layers = L.LayerGroup(), //layers contains all markers..
+    contained = [];          //makers in map boundingbox
+
+layers.eachLayer(function(l) {
+    if( l instanceof L.Marker && map.getBounds().contains(l.getLatLng()) )
+        contained.push(l);
+});
+
+
+  
 });
